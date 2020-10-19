4f3d2987e5724d895a83b0cead85d280e7d04dac
Ala Hamadi <ala.hamadi.01@gmail.com>
2020-10-20 00:06:30 +0200

    Updates

---
+++ precache-manifest.92bbe52401180d987dc22438fcb71672.js
@@ -0,0 +1,82 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "cea21d432e0ba7751761b7ca12943f13",
+    "url": "/MyPortfolio/index.html"
+  },
+  {
+    "revision": "bcfaa25c35330b406cd2",
+    "url": "/MyPortfolio/static/css/main.5ca60f80.chunk.css"
+  },
+  {
+    "revision": "7470fe5378675350373b",
+    "url": "/MyPortfolio/static/js/2.a86a63d4.chunk.js"
+  },
+  {
+    "revision": "d0b30a1909a753c393e134caf98fa9bb",
+    "url": "/MyPortfolio/static/js/2.a86a63d4.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "bcfaa25c35330b406cd2",
+    "url": "/MyPortfolio/static/js/main.389c10df.chunk.js"
+  },
+  {
+    "revision": "0d8bee11d1469486e213",
+    "url": "/MyPortfolio/static/js/runtime-main.7e73d9a0.js"
+  },
+  {
+    "revision": "aed4eefff2987b24714df88a552b6b1a",
+    "url": "/MyPortfolio/static/media/certificate1.aed4eeff.PNG"
+  },
+  {
+    "revision": "bbf192420e0f103844240a9fb80926c2",
+    "url": "/MyPortfolio/static/media/certificate2.bbf19242.PNG"
+  },
+  {
+    "revision": "f362b43dbda29f05aca4dede09ea530a",
+    "url": "/MyPortfolio/static/media/certificate3.f362b43d.PNG"
+  },
+  {
+    "revision": "878fac1dd5eef6b1433c0129f664b0ec",
+    "url": "/MyPortfolio/static/media/certificate4.878fac1d.PNG"
+  },
+  {
+    "revision": "cabbc3b44058ac67e0632d169a1f92cc",
+    "url": "/MyPortfolio/static/media/certificate5.cabbc3b4.PNG"
+  },
+  {
+    "revision": "ef544ee5f7b5ff0f73f2e7ddf3ba8cdf",
+    "url": "/MyPortfolio/static/media/certificate6.ef544ee5.PNG"
+  },
+  {
+    "revision": "e93547a72934d4883dba0da6754ac3e4",
+    "url": "/MyPortfolio/static/media/certificate7.e93547a7.PNG"
+  },
+  {
+    "revision": "6e4d89b4defcab3ac8c5924bdc99f6d4",
+    "url": "/MyPortfolio/static/media/certificate8.6e4d89b4.PNG"
+  },
+  {
+    "revision": "1dc35d25e61d819a9c357074014867ab",
+    "url": "/MyPortfolio/static/media/fontawesome-webfont.1dc35d25.ttf"
+  },
+  {
+    "revision": "25a32416abee198dd821b0b17a198a8f",
+    "url": "/MyPortfolio/static/media/fontawesome-webfont.25a32416.eot"
+  },
+  {
+    "revision": "c8ddf1e5e5bf3682bc7bebf30f394148",
+    "url": "/MyPortfolio/static/media/fontawesome-webfont.c8ddf1e5.woff"
+  },
+  {
+    "revision": "d7c639084f684d66a1bc66855d193ed8",
+    "url": "/MyPortfolio/static/media/fontawesome-webfont.d7c63908.svg"
+  },
+  {
+    "revision": "e6cf7c6ec7c2d6f670ae9d762604cb0b",
+    "url": "/MyPortfolio/static/media/fontawesome-webfont.e6cf7c6e.woff2"
+  },
+  {
+    "revision": "05a06ff244020ffa78f28b44be66d7a3",
+    "url": "/MyPortfolio/static/media/me.05a06ff2.jpg"
+  }
+]);
